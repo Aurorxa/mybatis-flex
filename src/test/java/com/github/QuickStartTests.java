bef5c80f8869ac5b61f37c170a9d8e62c319875e
许大仙 <1900919313@qq.com>
2023-12-08 20:01:25 +0800

    🎨: 快速开始

--- src/test/java/com/github/QuickStartTests.java
+++ src/test/java/com/github/QuickStartTests.java
@@ -33,4 +33,63 @@ public class QuickStartTests {
         Assertions.assertNotNull(account);
     }
 
+    @ParameterizedTest
+    @ValueSource(ints = {18,19})
+    public void testQuickStart2(int age) {
+
+        QueryWrapper queryWrapper = QueryWrapper
+                .create()
+                .select(ACCOUNT.DEFAULT_COLUMNS) // 默认字段，不包含逻辑删除字段或 large 等字段
+                .where(ACCOUNT.AGE.eq(age));
+
+        Account account = accountMapper.selectOneByQuery(queryWrapper);
+
+        log.info("{}", account);
+
+        Assertions.assertNotNull(account);
+        Assertions.assertNotNull(account.getId());
+        Assertions.assertNotNull(account.getUserName());
+        Assertions.assertNotNull(account.getAge());
+        Assertions.assertNotNull(account.getBirthday());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {18,19})
+    public void testQuickStart3(int age) {
+
+        QueryWrapper queryWrapper = QueryWrapper
+                .create()
+                .select(ACCOUNT.ALL_COLUMNS) // 查询所有字段，类似于 *
+                .where(ACCOUNT.AGE.eq(age));
+
+        Account account = accountMapper.selectOneByQuery(queryWrapper);
+
+        log.info("{}", account);
+
+        Assertions.assertNotNull(account);
+        Assertions.assertNotNull(account.getId());
+        Assertions.assertNotNull(account.getUserName());
+        Assertions.assertNotNull(account.getAge());
+        Assertions.assertNotNull(account.getBirthday());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {18,19})
+    public void testQuickStart4(int age) {
+
+        QueryWrapper queryWrapper = QueryWrapper
+                .create()
+                .select(ACCOUNT.AGE) // 查询指定字段
+                .where(ACCOUNT.AGE.eq(age));
+        Account account = accountMapper.selectOneByQuery(queryWrapper);
+
+        log.info("{}", account);
+
+        Assertions.assertNotNull(account);
+        Assertions.assertNull(account.getId());
+        Assertions.assertNull(account.getUserName());
+        Assertions.assertNotNull(account.getAge());
+        Assertions.assertNull(account.getBirthday());
+    }
+
 }
